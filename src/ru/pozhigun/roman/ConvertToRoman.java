a5698239c38030013740867d667e916d05c58e9a
Dmitriy <d.pozhigun@gmail.com>
2021-03-23 15:35:39 +0500

    implemented Alphabet, ConvertToRoman

---
+++ src/ru/pozhigun/roman/ConvertToRoman.java
@@ -0,0 +1,15 @@
+package ru.pozhigun.roman;
+
+public class ConvertToRoman extends Alphabet {
+
+    public String convert(int number) {
+        int l = getMap().floorKey(number);
+
+        if (number == l) {
+            return getMap().get(number);
+        }
+
+        return getMap().get(l) + convert(number - l);
+    }
+
+}
